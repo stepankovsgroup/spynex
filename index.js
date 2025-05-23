6f45a9e4394167263f6d8ebd86e10546047f2c95
stepankovsgroup <stepankov.artem@icloud.com>
2025-05-23 15:15:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
- <script>
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileMenu = document.getElementById('mobile-menu');
+document.addEventListener('DOMContentLoaded', () => {
+  const images = document.querySelectorAll('.gallery div');
+  // const modal = document.querySelector('#myModal');
+  // const closeBtn = modal.querySelector('.close-btn');
+  // const modalImage = modal.querySelector('.modal-img')
 
-    menuToggle.addEventListener('click', () => {
-      mobileMenu.classList.toggle('hidden');
-    });
-  </script>
+  // images.forEach((imageBlock) => {
+  //   imageBlock.addEventListener('click', () => {
+  //     const img = imageBlock.querySelector("img");
+  //     modal.style.display = 'flex';
+  //     modalImage.innerHTML = '';
+
+  //     const clonedImage = img.cloneNode(true);
+  //     modalImage.appendChild(clonedImage);
+  //   });
+  // })
+
+  // closeBtn.addEventListener('click', () => {
+  //   modal.style.display = 'none';
+  // });
+
+  // window.addEventListener('click', (event) => {
+  //   if (event.target === modal) {
+  //     modal.style.display = 'none';
+  //   }
+  // });
+  images.forEach((imageBlock) => {
+    imageBlock.addEventListener('click', () => {
+      const img = imageBlock.querySelector("img");
+      const text = img.getAttribute("alt");
+      const url = img.getAttribute("data-url");
+      const imgMask = imageBlock.querySelector(".img-mask");
+
+      console.log(imgMask)
+
+      if( imgMask ) {
+        imgMask.remove();
+      } else {
+        const div = document.createElement("div");
+        div.classList.add("img-mask");
+        const span = document.createElement("span");
+        span.textContent = text;
+        const link = document.createElement("a");
+        link.href = url;
+        link.textContent = "image link";
+
+        div.appendChild(span);
+        div.appendChild(link);
+
+        imageBlock.appendChild(div);
+      }
+    })
+  })
+});
