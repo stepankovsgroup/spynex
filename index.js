6117eac3c98e946eb8e222448320aef7c85a17f9
stepankovsgroup <stepankov.artem@icloud.com>
2025-06-01 10:26:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
  const closeBtn = modal.querySelector('.close-btn');
 const modalImage = modal.querySelector('.modal-img')
 
- images.forEach((imageBlock) => {
-  imageBlock.addEventListener('click', () => {
-    const img = imageBlock.querySelector("img");
+images.forEach((imageBlock) => {
+ imageBlock.addEventListener('click', () => {
+      const img = imageBlock.querySelector("img");
    modal.style.display = 'flex';
      modalImage.innerHTML = '';
 
-  const clonedImage = img.cloneNode(true);
-    modalImage.appendChild(clonedImage);
+   const clonedImage = img.cloneNode(true);
+     modalImage.appendChild(clonedImage);
    });
-   })
+  })
 
-  closeBtn.addEventListener('click', () => {
-   modal.style.display = 'none';
-   });
+ closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+ });
 
-   window.addEventListener('click', (event) => {
-     if (event.target === modal) {
-      modal.style.display = 'none';
-    }
-   });
+ window.addEventListener('click', (event) => {
+ if (event.target === modal) {
+    modal.style.display = 'none';
+ }
+ });
   images.forEach((imageBlock) => {
     imageBlock.addEventListener('click', () => {
       const img = imageBlock.querySelector("img");
